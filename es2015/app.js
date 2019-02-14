9e6911c2027acba8bb318dc4bed290cd1a56099b
Guillaume DG <Guillaume.dusseuxgicquel@gmail.com>
2019-02-14 14:02:06 +0100

    dernier commit

--- es2015/app.js
+++ es2015/app.js
@@ -111,4 +111,72 @@ console.log(temperature);
      console.log(freeTrip.toString());
 
 
-     
+     /* PROMISES, SET, MAP, ARROW FUNCTION */
+
+
+     class TripService {
+
+        constructor() {
+            
+            let trip1 = new Trip('paris', 'Paris', 'img/paris.jpg')
+            let trip2 = new Trip('nantes', 'Nantes', 'img/nantes.jpg')
+            let trip3 = new Trip('rio-de-janeiro', 'Rio de Janeiro', 'img/rio-de-janeiro.jpg')
+
+            let trips = new Set(); // création du set
+            trips.add(trip1);
+            trips.add(trip2);
+
+            console.log(trips.size); // 2
+            console.log(trips.has(trip1)); // true
+
+            trips.add(trip3);
+            console.log(trips.size); // 3
+
+            trips.delete(trip1);
+            console.log(trips.size); // 2  
+             
+        }
+
+
+        findByName(tripName) {
+    
+             return new Promise((resolve, reject) => {
+    
+                 setTimeout( () => {
+                     // ici l'exécution du code est asynchrone
+    
+                     // TODO utiliser resolve et reject en fonction du résultat de la recherche
+    
+                    
+
+
+
+                 }, 2000)
+            });
+        }
+    }
+
+
+    class PriceService {
+    
+        constructor() {
+
+            let prices = new Map(); // création d'un map
+            prices.set('paris', 100);
+            prices.set('rio-de-janeiro', 800); 
+
+        }
+    
+        findPriceByTripId(tripId) {
+    
+           return new Promise((resolve, reject) => {
+    
+                        setTimeout( () => {
+                            // ici l'exécution du code est asynchrone
+    
+                            // TODO utiliser resolve et reject en fonction du résultat de la recherche
+    
+                        }, 2000)
+                   });
+        }
+    }
